7ae36bb5444c72df9bf2c4c025b4ad4eccc93922
ezntek <eason@ezntek.com>
2025-06-30 12:26:12 +0800

    fix: code now compilable after upstream asv change

--- beanbricks.c
+++ beanbricks.c
@@ -19,10 +19,11 @@
 
 #include <raylib.h>
 
+#include "3rdparty/include/a_common.h"
+#include "3rdparty/include/a_string.h"
 #include "settings.h"
 
 #define RAYGUI_IMPLEMENTATION
-#include "3rdparty/include/asv.h"
 #include "3rdparty/include/raygui.h"
 
 #define VERSION "0.2.0-pre"
